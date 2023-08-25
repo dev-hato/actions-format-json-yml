5d9bdc5c0c1ef765a18a879a78865f523f99aa56
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2023-08-25 23:56:12 +0900

    CIのYAMLも実行対象に含める (#1074)
    * CIのYAMLも実行対象に含める
    
    * TOKEN指定
    
    * formatが間違ってたので直してあげたよ！ (#1076)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * super-linterによる指摘事項修正
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- scripts/action/format.sh
+++ scripts/action/format.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-find . -not -path './.github/workflows/*' \( -name '*.json' -o -name '*.geojson' \) -exec yq -i -o json {} \;
-find . -not -path './.github/workflows/*' \( -name '*.yaml' -o -name '*.yml' \) -exec yq -i {} \;
+find . \( -name '*.json' -o -name '*.geojson' \) -exec yq -i -o json {} \;
+find . \( -name '*.yaml' -o -name '*.yml' \) -exec yq -i {} \;
