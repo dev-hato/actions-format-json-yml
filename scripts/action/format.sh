ffb993e1f72ac0ce95fdfb1dd5262f02180fd9df
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2023-12-09 12:08:04 +0900

    前回format時と比べてdiffが出なくなるまでformatする (#1222)

--- scripts/action/format.sh
+++ scripts/action/format.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
-find . \( -name '*.json' -o -name '*.geojson' \) -exec yq -i -o json {} \;
-find . \( -name '*.yaml' -o -name '*.yml' \) -exec yq -i {} \;
+current_diff=""
+
+for _ in $(seq 10)
+do
+  find . \( -name '*.json' -o -name '*.geojson' \) -exec yq -i -o json {} \;
+  find . \( -name '*.yaml' -o -name '*.yml' \) -exec yq -i {} \;
+  git add -A
+  previous_diff="$current_diff"
+  current_diff="$(git diff --cached)"
+  if diff -u <(echo "$previous_diff") <(echo "$current_diff")
+  then
+    exit
+  fi
+done
