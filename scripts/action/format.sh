065a654943c28893a1db01fb38c596ff2e48d5ab
massongit <suzukimasaya428@gmail.com>
2024-02-06 21:10:54 +0900

    super-linterによる指摘事項修正

--- scripts/action/format.sh
+++ scripts/action/format.sh
@@ -2,15 +2,13 @@
 
 current_diff=""
 
-for _ in $(seq 10)
-do
-  find . \( -name '*.json' -o -name '*.geojson' \) -exec yq -i -o json {} \;
-  find . \( -name '*.yaml' -o -name '*.yml' \) -exec yq -i {} \;
-  git add -A
-  previous_diff="$current_diff"
-  current_diff="$(git diff --cached)"
-  if diff -u <(echo "$previous_diff") <(echo "$current_diff")
-  then
-    exit
-  fi
+for _ in $(seq 10); do
+	find . \( -name '*.json' -o -name '*.geojson' \) -exec yq -i -o json {} \;
+	find . \( -name '*.yaml' -o -name '*.yml' \) -exec yq -i {} \;
+	git add -A
+	previous_diff="$current_diff"
+	current_diff="$(git diff --cached)"
+	if diff -u <(echo "$previous_diff") <(echo "$current_diff"); then
+		exit
+	fi
 done
